3dd3494851a253cb5938625b870659fb0abde9fa
Igor Baiborodine <ibaiborodine@gmail.com>
2016-04-21 16:03:26 -0400

    Add static content dispatcher config

--- src/main/java/org/mybatis/jpetstore/config/WebConfig.java
+++ src/main/java/org/mybatis/jpetstore/config/WebConfig.java
@@ -1,37 +1,55 @@
 package org.mybatis.jpetstore.config;
 
+import static com.google.common.collect.Lists.newArrayList;
+
 import net.sourceforge.stripes.controller.StripesFilter;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.Servlet;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 /**
  * @author Igor Baiborodine
  */
 @Configuration
-public class StripesConfig {
+public class WebConfig {
 
   @Bean()
-  public Servlet dispatcherServlet() {
+  public Servlet staticContentDispatcher() {
+    return new DispatcherServlet();
+  }
+
+  @Bean
+  public ServletRegistrationBean staticContentDispatcherServletRegistration() {
+
+    ServletRegistrationBean registration = new ServletRegistrationBean(staticContentDispatcher());
+    registration.setName("staticContentDispatcher");
+    registration.addUrlMappings("*.html");
+    registration.setLoadOnStartup(1);
+    return registration;
+  }
+
+  @Bean()
+  public Servlet stripesDispatcher() {
     return new net.sourceforge.stripes.controller.DispatcherServlet();
   }
 
   @Bean
   public ServletRegistrationBean stripesDispatcherServletRegistration() {
 
-    ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet());
+    ServletRegistrationBean registration = new ServletRegistrationBean(stripesDispatcher());
     registration.setName("stripesDispatcher");
     registration.addInitParameter("javax.servlet.jsp.jstl.fmt.localizationContext", "StripesResources");
     registration.addUrlMappings("*.action");
-    registration.setLoadOnStartup(1);
+    registration.setLoadOnStartup(2);
     return registration;
   }
 
@@ -53,6 +71,16 @@ public class StripesConfig {
     registration.setMatchAfter(false);
 
     return registration;
+  }
+
+  @Bean
+  public WebMvcConfigurerAdapter forwardToIndex() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:index.html");
+      }
+    };
   }
 
 }
